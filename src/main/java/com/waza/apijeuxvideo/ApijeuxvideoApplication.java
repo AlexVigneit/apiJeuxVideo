4c280724ff4390ed1bc4c686638661df52aa5d9c
AlexVigneit <a.vigne@it-students.fr>
2024-06-18 09:49:04 +0200

    first commit

---
+++ src/main/java/com/waza/apijeuxvideo/ApijeuxvideoApplication.java
@@ -0,0 +1,13 @@
+package com.waza.apijeuxvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApijeuxvideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApijeuxvideoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/waza/apijeuxvideo/ApijeuxvideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waza.apijeuxvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApijeuxvideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
